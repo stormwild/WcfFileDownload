ca39cb4e2856b8a80078e1fcf938c0dde2a1f23e
Alexander Torrijos <artorrijos@gmail.com>
2020-10-13 21:21:46 +0800

    Added console client

---
+++ FileTransferConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using FileTransferConsole.FileTransferService;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTransferConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TransferServiceClient proxy = new TransferServiceClient();
+
+            Stream stream = new MemoryStream();
+            string filename = string.Empty;
+            
+            var result = proxy.DownloadFile(ref filename, out stream);
+
+            using (Stream file = File.Create(filename))
+            {
+                stream.CopyTo(file);
+            }
+
+            stream.Dispose();
+
+            Console.ReadLine();
+
+        }
+    }
+}
